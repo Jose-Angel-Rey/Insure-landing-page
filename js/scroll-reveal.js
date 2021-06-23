62d1e0c0ce230afc1af25efe77d6e6083b3b2cdb
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-06-23 07:09:54 -0400

    Update scroll-reveal.js

--- js/scroll-reveal.js
+++ js/scroll-reveal.js
@@ -2,10 +2,66 @@
 //      ScrollReveal animations
 // *******************************
 
-// -----   -----
-ScrollReveal().reveal(".fab", {
+// ----- Main content - We are differrent section -----
+ScrollReveal().reveal(".we-are-different__title", {
     duration: 2000,
-    delay: 500,
+    delay: 350,
+    origin: "bottom",
+    distance: "2rem",
+  });
+ScrollReveal().reveal(".we-are-different__article:first-of-type", {
+    duration: 2000,
+    delay: 550,
+    origin: "left",
+    distance: "3rem",
+  });
+  ScrollReveal().reveal(".we-are-different__article:nth-child(even)", {
+    duration: 2000,
+    delay: 550,
+    origin: "bottom",
+    distance: "3rem",
+  });
+ScrollReveal().reveal(".we-are-different__article:last-of-type", {
+    duration: 2000,
+    delay: 550,
+    origin: "right",
+    distance: "3rem",
+  });
+// ----- CTA  -----
+ScrollReveal().reveal(".cta__container", {
+    duration: 2000,
+    delay: 350,
+    origin: "bottom",
+    distance: "2rem",
+  });
+// ----- Main footer  -----
+ScrollReveal().reveal(".", {
+    duration: 2000,
+    delay: 350,
+    origin: "right",
+    distance: "2rem",
+  });
+ScrollReveal().reveal(".", {
+    duration: 2000,
+    delay: 350,
+    origin: "right",
+    distance: "2rem",
+  });
+ScrollReveal().reveal(".", {
+    duration: 2000,
+    delay: 350,
+    origin: "right",
+    distance: "2rem",
+  });
+ScrollReveal().reveal(".", {
+    duration: 2000,
+    delay: 350,
+    origin: "right",
+    distance: "2rem",
+  });
+ScrollReveal().reveal(".", {
+    duration: 2000,
+    delay: 350,
     origin: "right",
     distance: "2rem",
   });
